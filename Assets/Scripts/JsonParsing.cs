1b5cded6cf6661e3764defc8d6e156c51a2fc6e7
JustB0SS <dragonmount176@gmail.com>
2019-04-21 19:23:42 +0700

    Add json parser

---
+++ Assets/Scripts/JsonParsing.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class JsonParsing : MonoBehaviour
+{
+    private string json;
+
+    [SerializeField]
+    private Ticket ticket = new Ticket();
+
+    public Ticket[] Theme = new Ticket[28];
+
+    private void Start()
+    {
+        json = Application.streamingAssetsPath + "/questions.json";
+
+        if (File.Exists(json))
+        {
+            ticket = JsonUtility.FromJson<Ticket>(File.ReadAllText(json));
+            foreach (Questions question in ticket.Questions)
+            {
+                Theme[question.index - 1].Questions.Add(question);
+
+            }
+        }
+        else Debug.Log("NO load");
+    }
+
+}
---
+++ Assets/Scripts/Ticket.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public class Ticket
+{
+    public List<Questions> Questions = new List<Questions>();
+}
