1781403e0fa08af0e2408bc4cdff6cfbee8f04ca
by1ex <zlobin.aleksei95@gmail.com>
2019-05-22 18:34:34 +0700

    Add basic files with statistics functions.

--- Assets/Scripts/JsonParsing.cs
+++ Assets/Scripts/JsonParsing.cs
@@ -1,30 +1,98 @@
 ﻿using System.IO;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+
 
 public class JsonParsing : MonoBehaviour
 {
     private string json;
+    private string pathDownload;
+    private string path;
+
+    private Stats stats;
+
+    public string fileName = "questions.json";
 
     [SerializeField]
     private Ticket ticket = new Ticket();
 
-    public Ticket[] Theme = new Ticket[28];
+    public Ticket[] Theme = new Ticket[26];
+
+    IEnumerator load()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(pathDownload);
+        yield return www.SendWebRequest();
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log("Error");
+        }
+        else
+        {
+            json = www.downloadHandler.text;
+            File.WriteAllText(path, json);
+        }
+
+        ticket = JsonUtility.FromJson<Ticket>(json);
+        foreach (Questions question in ticket.Questions)
+        {
+            Theme[question.index - 1].Questions.Add(question);
+        }
+
+        yield return new WaitForSeconds(0.1f);
+    }
+
+
+    public void WriteStats()
+    {
+        Ticket ticketToStats = new Ticket();
+        for (int i = 0; i < 26; i++)
+        {
+            foreach (Questions question in Theme[i].Questions)
+            {
+                ticketToStats.Questions.Add(question);
+            }
+        }
+        string ToJson = JsonUtility.ToJson(ticketToStats, true);
+        File.WriteAllText(path, ToJson);
+        stats.Upload(Theme);
+    }
 
     private void Start()
     {
-        Screen.SetResolution(500, 800,false);
-        json = Application.streamingAssetsPath + "/questions.json";
+#if UNITY_ANDROID && !UNITY_EDITOR
+        path = Path.Combine(Application.persistentDataPath, fileName);
+#else
+        path = Path.Combine(Application.dataPath, fileName);
+#endif
+        stats = GetComponent<Stats>();
+        pathDownload = Path.Combine(Application.streamingAssetsPath, fileName);
 
-        if (File.Exists(json))
+        if (File.Exists(path))
         {
-            ticket = JsonUtility.FromJson<Ticket>(File.ReadAllText(json));
+            json = File.ReadAllText(path);
+            ticket = JsonUtility.FromJson<Ticket>(json);
             foreach (Questions question in ticket.Questions)
             {
                 Theme[question.index - 1].Questions.Add(question);
-
             }
         }
-        else Debug.Log("NO load");
-    }
+        else
+        {
 
+#if UNITY_ANDROID && !UNITY_EDITOR
+        StartCoroutine("load");
+#else
+            json = File.ReadAllText(pathDownload);
+            File.WriteAllText(path, json);
+            ticket = JsonUtility.FromJson<Ticket>(json);
+            foreach (Questions question in ticket.Questions)
+            {
+                Theme[question.index - 1].Questions.Add(question);
+            }
+#endif
+        }
+        WriteStats();
+    }
 }
