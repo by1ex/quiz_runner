ebe58e0ee9703f11e75e6124d728d298711badc1
JustB0SS <dragonmount176@gmail.com>
2019-05-26 14:18:47 +0700

    Add tests loading json file

---
+++ Assets/Tests/Editor/TestJsonLoadFile.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestJsonLoadFile
+{
+    [Test]
+    public void LoadSuccessTest()
+    {
+        JsonParsing JsonParser = new JsonParsing();
+        int expectedAnswer = JsonParser.PathJsonTest("questions.json");
+        Assert.AreEqual(1, expectedAnswer);
+    }
+
+    [Test]
+    public void LoadUnsuccessTest()
+    {
+        JsonParsing JsonParser = new JsonParsing();
+        int expectedAnswer = JsonParser.PathJsonTest("questions1.json");
+        Assert.AreEqual(2, expectedAnswer);
+    }
+}
